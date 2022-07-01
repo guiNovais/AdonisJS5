c6cd0d6e350bce2c8b3ad95a9ba0a00528d3214e
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2022-07-01 00:02:16 -0300

    User POST API

---
+++ roleplay-api/test/users/users.spec.ts
@@ -0,0 +1,14 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+test.group('User', () => {
+  test.only('it should create a user', async () => {
+    const userPayload = {
+      email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+    }
+    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+  })
+})
