9fe5bbc4f7afd3eff50589a3967f32de0c3dbd80
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2022-07-02 01:55:18 -0300

    Persistir User no banco de dados

--- roleplay-api/test/users/users.spec.ts
+++ roleplay-api/test/users/users.spec.ts
@@ -3,12 +3,17 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('User', () => {
-  test.only('it should create a user', async () => {
+  test.only('it should create a user', async (assert) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
       password: 'test',
     }
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'Id undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.password, userPayload.password)
   })
 })
