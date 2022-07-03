badcba27f8d187f54c731477c1acb22a1a6808e9
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2022-07-03 02:54:26 -0300

    Aplicar regra de email unico

--- roleplay-api/test/users/users.spec.ts
+++ roleplay-api/test/users/users.spec.ts
@@ -1,10 +1,11 @@
 import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('User', (group) => {
-  test.only('it should create a user', async (assert) => {
+  test('it should create a user', async (assert) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
@@ -16,6 +17,14 @@ test.group('User', (group) => {
     assert.equal(body.user.email, userPayload.email)
     assert.equal(body.user.username, userPayload.username)
     assert.notExists(body.user.password)
+  })
+
+  test('it should return 409 when email is already in use', async () => {
+    const { email } = await UserFactory.create()
+    await supertest(BASE_URL)
+      .post('/users')
+      .send({ email, username: 'test', password: 'test' })
+      .expect(409)
   })
 
   group.beforeEach(async () => {
