5178ce4cc114b81af359d917d2020da3d5d2b995
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2022-08-25 10:38:39 -0300

    Gerar token no PasswordController

--- roleplay-api/test/users/passwords.spec.ts
+++ roleplay-api/test/users/passwords.spec.ts
@@ -8,7 +8,7 @@ import { UserFactory } from '../../database/factories/index'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Password', (group) => {
-  test.only('It shoud send an email with forgot password instructions', async (assert) => {
+  test('It shoud send an email with forgot password instructions', async (assert) => {
     Mail.trap((message) => {
       assert.deepEqual(message.to, [{ address: user.email }])
       assert.deepEqual(message.from, { address: 'no-reply@roleplay.com' })
@@ -24,6 +24,20 @@ test.group('Password', (group) => {
       })
       .expect(204)
     Mail.restore()
+  })
+
+  test.only('it should create a reset password token', async (assert) => {
+    const user = await UserFactory.create()
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPasswordUrl: `url`,
+      })
+      .expect(204)
+
+    const tokens = await user.related('tokens').query()
+    assert.isNotEmpty(tokens)
   })
 
   group.beforeEach(async () => {
