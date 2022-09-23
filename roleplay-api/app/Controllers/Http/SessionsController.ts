def72538234e5086869e55aa14c6f0cea1531192
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2022-09-23 07:17:58 -0300

    Desautenticar usuário

--- roleplay-api/app/Controllers/Http/SessionsController.ts
+++ roleplay-api/app/Controllers/Http/SessionsController.ts
@@ -8,4 +8,9 @@ export default class SessionsController {
     })
     return response.created({ user: auth.user, token })
   }
+
+  public async destroy({ response, auth }: HttpContextContract) {
+    await auth.logout()
+    return response.ok({})
+  }
 }
--- roleplay-api/test/users/sessions.spec.ts
+++ roleplay-api/test/users/sessions.spec.ts
@@ -48,6 +48,49 @@ test.group('Sessions', (group) => {
     assert.equal(body.message, 'invalid credentials')
   })
 
+  test('it should return 200 when user signs out', async () => {
+    const plainPassword = 'pass'
+    const { email } = await UserFactory.merge({ password: plainPassword }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    const token = body.token.token
+
+    await supertest(BASE_URL)
+      .delete('/sessions')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+      .expect(200)
+  })
+
+  test('it should revoke token when user signs out', async (assert) => {
+    const plainPassword = 'pass'
+    const { email } = await UserFactory.merge({ password: plainPassword }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    const apiToken = body.token
+    const tokensBeforeSignOut = await Database.query().select('*').from('api_tokens')
+    assert.isNotEmpty(tokensBeforeSignOut)
+
+    await supertest(BASE_URL)
+      .delete('/sessions')
+      .set('Authorization', `Bearer ${apiToken.token}`)
+      .send()
+      .expect(200)
+
+    const tokenAfterSignOut = await Database.query()
+      .select('*')
+      .from('api_tokens')
+      .where('token', apiToken.token)
+
+    assert.isEmpty(tokenAfterSignOut)
+  })
+
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
