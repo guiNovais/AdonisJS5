5f18ca07ada7f944e5dc725e8a477aeee4ef94cb
Guilherme Novais <guilhermenovais.sh@gmail.com>
2022-11-08 07:19:52 -0300

    Aceitar requisição de ingresso a uma mesa

--- roleplay-api/test/groups/group_request.spec.ts
+++ roleplay-api/test/groups/group_request.spec.ts
@@ -130,6 +130,65 @@ test.group('Group Request', (group) => {
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 422)
   })
+
+  test('it should accept a group request', async (assert) => {
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    const response = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests/${body.groupRequest.id}/accept`)
+      .expect(200)
+
+    assert.exists(response.body.groupRequest, 'GroupRequest undefined')
+    assert.equal(response.body.groupRequest.userId, user.id)
+    assert.equal(response.body.groupRequest.groupId, group.id)
+    assert.equal(response.body.groupRequest.status, 'ACCEPTED')
+
+    await group.load('players')
+    assert.isNotEmpty(group.players)
+    assert.equal(group.players.length, 1)
+    assert.equal(group.players[0].id, user.id)
+  })
+
+  test('it should return 404 when providing an unexistent group', async (assert) => {
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    const response = await supertest(BASE_URL)
+      .post(`/groups/123/requests/${body.groupRequest.id}/accept`)
+      .expect(404)
+
+    assert.equal(response.body.code, 'BAD_REQUEST')
+    assert.equal(response.body.status, 404)
+  })
+
+  test('it should return 404 when providing an unexistent group request', async (assert) => {
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    const response = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests/123/accept`)
+      .expect(404)
+
+    assert.equal(response.body.code, 'BAD_REQUEST')
+    assert.equal(response.body.status, 404)
+  })
+
   group.before(async () => {
     const plainPassword = 'pass'
     user = await UserFactory.merge({ password: plainPassword }).create()
